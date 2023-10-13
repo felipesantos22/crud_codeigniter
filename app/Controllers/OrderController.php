b4c08dfaaa7b2d246d1df7244760e174cb2324e7
felipesantos22 <felipevs2018@gmail.com>
2023-10-13 17:41:12 -0300

    feat: inserindo relacionamento entre pedido e cliente

--- app/Controllers/OrderController.php
+++ app/Controllers/OrderController.php
@@ -10,38 +10,28 @@ class OrderController extends BaseController
 {
     use ResponseTrait;
 
-    //Create Order
-    // public function createOrder()
-    // {
-    //     $model = new OrderModel();
-    //     $data = $this->request->getJSON();
-
-    //     if ($model->insert($data)) {
-    //         $response = [
-    //             'status'   => 201,
-    //             'error'    => null,
-    //             'messages' => [
-    //                 'success' => 'Dados salvos'
-    //             ],
-    //             'Order' => $data
-    //         ];
-    //         return $this->respond($response);
-    //     }
-
-    //     return $this->fail($model->errors());
-    // }
-
+    // Create Order
     public function createOrder()
     {
         $model = new OrderModel();
-        $data = [
-            'status' => $this->request->getPost('status'),
-            'client_id' => $this->request->getPost('client_id'),
-        ];
-        $model->insert($data);
+        $data = $this->request->getJSON();
 
-        return $this->respond($model);
+        if ($model->insert($data)) {
+            $response = [
+                'status'   => 201,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Dados salvos'
+                ],
+                'Order' => $data
+            ];
+            return $this->respond($response);
+        }
+
+        return $this->fail($model->errors());
     }
+
+
 
     // List All Order
     public function readOrder()
